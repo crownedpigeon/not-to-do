eaf56ed33415c28cd1f86ab781d9a79335b4d788
crownedpigeon <emily.rigole@gmail.com>
2021-01-11 15:32:19 -0500

    working on cool complete animation WIP

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import "./todo.css"
 
 const Todo = ({todo, removeTodo}) => {
+    const [complete, setComplete] = useState('')
+
     return (
         <>
-        <h2 className={todo.priority}>{todo.text} <span onClick={() => removeTodo(todo)}>x</span></h2>
+        <h2 className={`${todo.priority} ${complete}`}>{todo.text} <span onClick={() => {setComplete('complete'); /*set timeout for complete animition, then remove todo*/}}><span>✓</span>&nbsp;&nbsp;&nbsp;<span onClick={() => removeTodo(todo)}>x</span> </span> </h2>
         </>
     )
 }
